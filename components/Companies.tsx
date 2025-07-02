d5ddfe1d23a1808424746c13fee2c7ffcc073d9f
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2025-07-02 00:16:03 -0300

    update companies

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -4,9 +4,25 @@ export default function Companies() {
   const logos = [
     { src: "/assets/logos/amazon.png", alt: "Amazon" },
     { src: "/assets/logos/google.png", alt: "Google" },
-    { src: "/assets/logos/netflix.png", alt: "Netflix" },
-    { src: "/assets/logos/meta.png", alt: "Meta" },
-    { src: "/assets/logos/apple.png", alt: "Apple" },
+    { src: "/assets/logos/ibm.png", alt: "IBM" },
+    { src: "/assets/logos/magnet.png", alt: "Magnet Forensics" },
+    { src: "/assets/logos/arup.png", alt: "Arup" },
+    { src: "/assets/logos/dayforce.webp", alt: "Dayforce" },
+    { src: "/assets/logos/telus.png", alt: "Telus" },
+    { src: "/assets/logos/nasdaq.png", alt: "Nasdaq" },
+    { src: "/assets/logos/ansys.png", alt: "Ansys" },
+    { src: "/assets/logos/cohere.png", alt: "Cohere" },
+    { src: "/assets/logos/cpp.png", alt: "CPPIB" },
+    { src: "/assets/logos/doordash.png", alt: "DoorDash" },
+    { src: "/assets/logos/ea.svg", alt: "EA" },
+    { src: "/assets/logos/shopify.png", alt: "Shopify" },
+    { src: "/assets/logos/robinhood.svg", alt: "Robinhood" },
+    { src: "/assets/logos/1password.svg", alt: "1Password" },
+    { src: "/assets/logos/sentry.png", alt: "Sentry" },
+    { src: "/assets/logos/super.com.png", alt: "Super.com" },
+    { src: "/assets/logos/bloomberg.webp", alt: "Bloomberg" },
+    { src: "/assets/logos/yelp.svg", alt: "Yelp" },
+    // { src: "/assets/logos/genesys.png", alt: "Genesys" },
   ];
 
   return (
@@ -16,7 +32,8 @@ export default function Companies() {
           <div className="w-full lg:w-6/12 px-4">
             <h1 className="text-gray-900 text-3xl font-bold mb-4">
               {/* Our members have gotten offers from these companies */}
-              Get offers from these companies
+              {/* Get offers from these companies */}
+              Our members have gotten offers and interviews from these companies
             </h1>
           </div>
         </div>
